42174ce1a3c0c513d8cb18ca415805a502254f1d
Eve <hello@eve.lc>
2021-11-23 18:00:13 -0500

    Perathax changes pt 2

--- routines/rolimons.js
+++ routines/rolimons.js
@@ -1,24 +1,26 @@
 import * as roli from '../api/rolimons.js'
 import obj from '../modules/objects.js'
-import perathax from '../modules/perathax.js'
+import { calcPerathax } from '../modules/perathax.js'
 
 let firedCount = 0
 async function doFirst() {
     if (firedCount > 0) return
+    await perathaxBehavior()
+}
 
-    obj.PerathaxList = perathax(await roli.getTable())
+async function perathaxBehavior() {
+    obj.PerathaxList = calcPerathax(await roli.getTable())
 }
 
 async function routine() {
     await doFirst()
     if (firedCount == 600) {
-        firedCount = 1
-        obj.PerathaxList = perathax(await roli.getTable())
-        return
+        firedCount = 0
+        await perathaxBehavior()
     }
 
-    firedCount += 1
     obj.ItemsList = await roli.getItems()
+    firedCount += 1
 }
 
 export default routine
