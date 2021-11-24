fbd9ce82523c943a393683be10f27a3fe39146aa
Eve <hello@eve.lc>
2021-11-23 20:43:38 -0500

    Add getreseller api method

--- api/roblox/market.js
+++ api/roblox/market.js
@@ -17,6 +17,25 @@ export async function getSalesHistory(id) {
         .then(resp => resp.json())
 }
 
+/*
+{
+    "userAssetId":452981782,
+    "seller": {
+        "id":453246493,
+        "type":"User",
+        "name":"unknownrealities"
+    },
+    "price":92887,
+    "serialNumber":null
+}
+*/
+export async function getResellers(id) {
+    let resp = await request(`https://economy.roblox.com/v1/assets/${id}/resellers?cursor=&limit=10`)
+        .then(resp => resp.json())    
+    
+    return resp.data
+}
+
 export async function getPlayerInventory(id) {
     return await paginate(
         `https://inventory.roblox.com/v1/users/${id}/assets/collectibles?sortOrder=Asc&limit=100`
