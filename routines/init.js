388ab76f4e9a982f05e3edf816fcb1570bf693fb
Awesomerly <Awesomerly@users.noreply.github.com>
2021-11-28 17:43:07 -0500

    Trade bot decline routine

--- routines/init.js
+++ routines/init.js
@@ -3,18 +3,20 @@ import { setIntervalAsync } from 'set-interval-async/dynamic/index.js'
 import playerInfoRoutine from './playerInfo.js'
 import roliRoutine from './rolimons.js'
 import getSnipes from './snipes.js'
+import declineBots from './trades.js'
 
-async function start(fn, ms) {
+async function start(fn, sec) {
     await fn()
-    setIntervalAsync(fn, ms)
+    setIntervalAsync(fn, sec * 1000)
 }
 
 // TODO: ERROR HANDLING BASED OFF THE FAULTY COMPONENT
 
 async function startEverything() {
-        await start(playerInfoRoutine, 60000)
-        await start(roliRoutine, 20000)
-        await start(getSnipes, 800)
+        await start(playerInfoRoutine, 60)
+        await start(roliRoutine, 20)
+        await start(getSnipes, 0.79)
+        await start(declineBots, 60 * 5)
 }
 
 export default startEverything
