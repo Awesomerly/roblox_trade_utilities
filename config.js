9b4d9262b15746e1459f9ef2394cc0cedf83eeb3
Awesomerly <Awesomerly@users.noreply.github.com>
2021-12-01 00:19:00 -0500

    Config File Moment

---
+++ config.js
@@ -0,0 +1,18 @@
+const config = 
+{
+    snipes: {
+        minPercent: 40,
+        maxPercent: 70,
+        // min percentage for price change to be logged
+        displayPercent: 0,
+
+        // will there be RAP items included from the request?
+        snipeRap: true,
+
+        // range of items from perathax to send:
+        peraRange: [ 50, 150 ]
+    }
+
+}
+
+export default config
