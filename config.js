7bf4693ddb2547da406faa48b40b7fafaf2986e1
Eve S <janedoe@example.com>
2021-12-20 19:19:09 -0500

    Add sell thresholds and item keepers

--- config.js
+++ config.js
@@ -1,22 +1,56 @@
-const config = 
+const config =
 {
-    selling: {
-        enabled: true,
+    items: {
+        keep: {
+            serials: [1, 69],
+            assetIds: [
+                1323367, // IH
+                1073690, // JJ
+                2215797, // EH
+                1193866, // CC
+                1286490, // DB
+                3271938, // ring
+                5162497, // HT
+                319655275, // Undersmeller
+                7893468574, // Zara
+            ],
+            uaids: [
+                1830733382 // CTH
+            ],
+            //fiveDigitUaids: true,
+        }
     },
 
+    // Item Selling Config
+    selling: {
+        enabled: true,
+
+        /* Thresholds determine limits on sale prices.
+
+           The seller always lists items above min,
+           above med if found within medSearch listings
+           other than your own, and below max.
+        */
+        threshold: {
+            min: 1,
+            max: 4,
+        }
+    },
+
+    // Snipe Bot Config
     snipes: {
         enabled: true,
 
         minPercent: 40,
-        maxPercent: 70,
+        maxPercent: 75,
         // min percentage for price change to be logged
         displayPercent: 0,
 
         // will there be RAP items included from the request?
-        snipeRap: true,
+        snipeRap: false,
 
         // range of items from perathax to send:
-        peraRange: [ 50, 150 ]
+        peraRange: [ 10, 110 ]
     }
 }
 
