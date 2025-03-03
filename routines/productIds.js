49c6c3ea5aefcc07e3f32a742fbd286acaf36035
Awesomerly <Awesomerly@users.noreply.github.com>
2025-03-03 15:48:33 -0500

    Update product id link
    (probably still broken)

--- routines/productIds.js
+++ routines/productIds.js
@@ -2,7 +2,7 @@ import obj from '../modules/objects.js'
 
 import fetch from 'node-fetch'
 
-const url = 'https://gist.githubusercontent.com/codetariat/4281d2fdfb7baefe0a1ce1ed5d8679c0/raw/'
+const url = 'https://gist.githubusercontent.com/codet-t/4281d2fdfb7baefe0a1ce1ed5d8679c0/raw/'
 
 async function routine() {
 
